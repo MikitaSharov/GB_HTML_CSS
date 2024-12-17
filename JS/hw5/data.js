981b759fca7c3b7372bf99fde84aeaaa7ec889e4
Nikita Sharov <lliapik@gmail.com>
2024-12-17 13:50:35 +0300

    hw5

---
+++ JS/hw5/data.js
@@ -0,0 +1,122 @@
+const userData = {
+    "results": [
+      {
+        "gender": "male",
+        "name": {
+          "title": "Mr",
+          "first": "Çetin",
+          "last": "Korol"
+        },
+        "location": {
+          "street": {
+            "number": 7670,
+            "name": "Talak Göktepe Cd"
+          },
+          "city": "Hatay",
+          "state": "Sakarya",
+          "country": "Turkey",
+          "postcode": 64379,
+          "coordinates": {
+            "latitude": "72.2536",
+            "longitude": "-156.9535"
+          },
+          "timezone": {
+            "offset": "-2:00",
+            "description": "Mid-Atlantic"
+          }
+        },
+        "email": "cetin.korol@example.com",
+        "login": {
+          "uuid": "386c46a9-b656-41e0-b630-33ee287daa5d",
+          "username": "brownbird280",
+          "password": "hahahaha",
+          "salt": "SAuzd4cp",
+          "md5": "8e48d91b28b63f3abd60a799349400e9",
+          "sha1": "28f9e9a619532d69597126ec55bdb7a701853a72",
+          "sha256": "e4daa4a896495cacbc6be0dd1a48d5ac058ca829cb166d203cae3946daee0270"
+        },
+        "dob": {
+          "date": "1955-11-17T20:42:05.570Z",
+          "age": 69
+        },
+        "registered": {
+          "date": "2021-02-10T05:56:19.406Z",
+          "age": 3
+        },
+        "phone": "(154)-866-3800",
+        "cell": "(258)-223-2493",
+        "id": {
+          "name": "",
+          "value": null
+        },
+        "picture": {
+          "large": "https://randomuser.me/api/portraits/men/89.jpg",
+          "medium": "https://randomuser.me/api/portraits/med/men/89.jpg",
+          "thumbnail": "https://randomuser.me/api/portraits/thumb/men/89.jpg"
+        },
+        "nat": "TR"
+      },
+      {
+        "gender": "male",
+        "name": {
+          "title": "Mr",
+          "first": "Ali",
+          "last": "Aydan"
+        },
+        "location": {
+          "street": {
+            "number": 6319,
+            "name": "Istiklal Cd"
+          },
+          "city": "Kırşehir",
+          "state": "Adana",
+          "country": "Turkey",
+          "postcode": 50987,
+          "coordinates": {
+            "latitude": "72.4641",
+            "longitude": "-141.2070"
+          },
+          "timezone": {
+            "offset": "+3:00",
+            "description": "Baghdad, Riyadh, Moscow, St. Petersburg"
+          }
+        },
+        "email": "ali.aydan@example.com",
+        "login": {
+          "uuid": "028a7707-4080-46d0-83b6-c8de823a652f",
+          "username": "silvergorilla809",
+          "password": "chrome",
+          "salt": "qcFcx2bc",
+          "md5": "8509fa9bcc91a5ee6876b147a8123321",
+          "sha1": "35acf3979f2f4f7dbda2ed3e734dbe205e4e0e0c",
+          "sha256": "edcf16038b8326d29c3bdc8ead2daf83d4a8df6b82652d46e706d48b6ca8cbf2"
+        },
+        "dob": {
+          "date": "1976-02-11T15:32:10.001Z",
+          "age": 48
+        },
+        "registered": {
+          "date": "2019-11-11T05:45:59.389Z",
+          "age": 5
+        },
+        "phone": "(512)-633-1991",
+        "cell": "(820)-574-4400",
+        "id": {
+          "name": "",
+          "value": null
+        },
+        "picture": {
+          "large": "https://randomuser.me/api/portraits/men/88.jpg",
+          "medium": "https://randomuser.me/api/portraits/med/men/88.jpg",
+          "thumbnail": "https://randomuser.me/api/portraits/thumb/men/88.jpg"
+        },
+        "nat": "TR"
+      }
+    ],
+    "info": {
+      "seed": "ce9c581f5a2cfcc2",
+      "results": 1,
+      "page": 1,
+      "version": "1.4"
+    }
+  }
